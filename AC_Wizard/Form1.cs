c92a6a5d661b8e9619657a5ab6198e18c260dbb2
Andrei <46579508+Andreisgl@users.noreply.github.com>
2022-11-07 18:25:40 -0300

    change tab settings

--- AC_Wizard/Form1.cs
+++ AC_Wizard/Form1.cs
@@ -1,59 +1,63 @@
 namespace AC_Wizard
 {
-    using System;
-    using System.Diagnostics;
-    using System.IO;
-    using System.Text;
-    public partial class Form1 : Form
-    {
-        Project_Managing Proj_Mng = new();
-        public Form1()
-        {
-            InitializeComponent();
-        }
+	using System;
+	using System.Diagnostics;
+	using System.IO;
+	using System.Text;
+	public partial class Form1 : Form
+	{
+		Project_Managing Proj_Mng = new();
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            Proj_Mng.Check_folders();
-            projectBrowserDialog1.InitialDirectory = Proj_Mng.Get_Project_Folder();
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			Proj_Mng.Check_folders();
+			projectBrowserDialog1.InitialDirectory = Proj_Mng.Get_Project_Folder();
 
-            btnOpenFile.Enabled = false;
-        }
+			btnOpenFile.Enabled = false;
+		}
 
-        private void btnOpenFile_Click(object sender, EventArgs e)
-        {
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                string[] file_name = new string[(openFileDialog1.FileNames.Length)];
-                file_name = openFileDialog1.FileNames;
-                Proj_Mng.Import_File_toRoot(file_name);
-            }
-        }
+		private void btnOpenFile_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				string[] file_name = new string[(openFileDialog1.FileNames.Length)];
+				file_name = openFileDialog1.FileNames;
+				Proj_Mng.Import_File_toRoot(file_name);
+				
+				listView1.Items.Add("");
+			}
+			
+		}
 
-        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
-        {          
-            //Process.Start(openFileDialog1.FileName);
-        }
+		private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+		{      
+			//Process.Start(openFileDialog1.FileName);
+		}
 
 
-        private string project_path = "";
-        private void btnOpenProject_Click(object sender, EventArgs e)
-        {
-            if (projectBrowserDialog1.ShowDialog() == DialogResult.OK)
-            {
-                project_path = projectBrowserDialog1.SelectedPath;
-                
-                
-                if(Proj_Mng.Open_Project(project_path))
-                {
-                    btnOpenFile.Enabled = true;
-                }
-                else
-                {
-                    btnOpenFile.Enabled = false;
-                }
-            }
-        }
-        
-    }
+		private string project_path = "";
+		private void btnOpenProject_Click(object sender, EventArgs e)
+		{
+			if (projectBrowserDialog1.ShowDialog() == DialogResult.OK)
+			{
+				project_path = projectBrowserDialog1.SelectedPath;
+				
+				
+				if(Proj_Mng.Open_Project(project_path))
+				{
+					btnOpenFile.Enabled = true;
+				}
+				else
+				{
+					btnOpenFile.Enabled = false;
+				}
+			}
+		}
+
+
+	}
 }
