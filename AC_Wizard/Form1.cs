f918bc3ff5979651d4115712fcfc1dc6122565d4
Andrei <46579508+Andreisgl@users.noreply.github.com>
2022-11-06 21:46:51 -0300

    progress

--- AC_Wizard/Form1.cs
+++ AC_Wizard/Form1.cs
@@ -2,6 +2,8 @@ namespace AC_Wizard
 {
     using System;
     using System.Diagnostics;
+    using System.IO;
+    using System.Text;
     public partial class Form1 : Form
     {
         public Form1()
@@ -11,6 +13,7 @@ namespace AC_Wizard
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Check_folders();
             btnOpenFile.Enabled = false;
         }
 
@@ -31,13 +34,55 @@ namespace AC_Wizard
             //Process.Start(openFileDialog1.FileName);
         }
 
+        private string project_path = "";
+        private string PROJECT_FOLDER = "Projects";
+        private void Check_folders()
+        {
+            string cwd = Directory.GetCurrentDirectory();
+            PROJECT_FOLDER = cwd + "\\" + PROJECT_FOLDER;
+            if(!Directory.Exists(PROJECT_FOLDER))
+            {
+                Directory.CreateDirectory(PROJECT_FOLDER);
+            }
+            projectBrowserDialog1.InitialDirectory = PROJECT_FOLDER;
+        }
         private void btnOpenProject_Click(object sender, EventArgs e)
         {
             if (projectBrowserDialog1.ShowDialog() == DialogResult.OK)
             {
-                var path = projectBrowserDialog1.SelectedPath;
-                label2.Text = path;
+                project_path = projectBrowserDialog1.SelectedPath;
+                label2.Text = PROJECT_FOLDER;
                 btnOpenFile.Enabled = true;
+                if (Is_Project(project_path))
+                {
+                    label2.Text = "YAY!";
+                }
+                else
+                {
+                    label2.Text = "NOO!";
+                }
+            }
+        }
+        private bool Is_Project(string path)
+        {
+            string project_info_file;
+            string project_info_filename = "project_info";
+            string project_info_file_extension = ".ACWProj";
+            project_info_filename += project_info_file_extension;
+
+            project_info_file = path + "\\" + project_info_filename;
+
+            label2.Text = project_info_file;
+
+            if (File.Exists(project_info_file))
+            {
+                //Console.WriteLine("Project file exists!");
+                return true;
+            }
+            else
+            {
+                //Console.WriteLine("Project file does not exist!");
+                return false;
             }
         }
     }
