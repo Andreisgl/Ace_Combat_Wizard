bf2f461ead6f4755bf5da28c3319178c0d6d5afd
Andrei <46579508+Andreisgl@users.noreply.github.com>
2022-11-07 00:40:58 -0300

    transfer functionality to Project_Managing

--- AC_Wizard/Project_Checking.cs
+++ AC_Wizard/Project_Checking.cs
@@ -8,25 +8,6 @@ namespace AC_Wizard
         {
         }
 
-        public bool Is_Project(string path)
-        {
-            string project_info_file;
-            string project_info_filename = "project_info";
-            string project_info_file_extension = ".ACWProj";
-            project_info_filename += project_info_file_extension;
-
-            project_info_file = path + "\\" + project_info_filename;
-
-            if (File.Exists(project_info_file))
-            {
-                //Console.WriteLine("Project file exists!");
-                return true;
-            }
-            else
-            {
-                //Console.WriteLine("Project file does not exist!");
-                return false;
-            }
-        }
+        
     }
 }
---
+++ AC_Wizard/Project_Managing.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AC_Wizard
+{
+	public class Project_Managing
+	{
+		readonly string project_info_filename = "project_info";
+		readonly string project_info_file_extension = ".ACWProj";
+
+		string project_info_file = "";
+
+
+		public Project_Managing()
+		{
+			project_info_filename += project_info_file_extension;
+		}
+
+		public bool Is_Project(string path)
+		{
+			project_info_file = path + "\\" + project_info_filename;
+			if (File.Exists(project_info_file))
+			{
+				//Console.WriteLine("Project file exists!");
+				return true;
+			}
+			else
+			{
+				//Console.WriteLine("Project file does not exist!");
+				return false;
+			}
+		}
+
+		public void Return_Project_Data(string project_path)
+        {
+
+        }
+	}
+}
