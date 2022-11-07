9add70f4246dba5d5ce76099fe237fb311985bd6
Andrei <46579508+Andreisgl@users.noreply.github.com>
2022-11-06 21:56:45 -0300

    Update Form1.cs

--- AC_Wizard/Form1.cs
+++ AC_Wizard/Form1.cs
@@ -48,12 +48,13 @@ namespace AC_Wizard
         }
         private void btnOpenProject_Click(object sender, EventArgs e)
         {
+            var Proj_Check = new Project_Checking();
             if (projectBrowserDialog1.ShowDialog() == DialogResult.OK)
             {
                 project_path = projectBrowserDialog1.SelectedPath;
                 label2.Text = PROJECT_FOLDER;
                 btnOpenFile.Enabled = true;
-                if (Is_Project(project_path))
+                if (Proj_Check.Is_Project(project_path))
                 {
                     label2.Text = "YAY!";
                 }
@@ -63,27 +64,6 @@ namespace AC_Wizard
                 }
             }
         }
-        private bool Is_Project(string path)
-        {
-            string project_info_file;
-            string project_info_filename = "project_info";
-            string project_info_file_extension = ".ACWProj";
-            project_info_filename += project_info_file_extension;
-
-            project_info_file = path + "\\" + project_info_filename;
-
-            label2.Text = project_info_file;
-
-            if (File.Exists(project_info_file))
-            {
-                //Console.WriteLine("Project file exists!");
-                return true;
-            }
-            else
-            {
-                //Console.WriteLine("Project file does not exist!");
-                return false;
-            }
-        }
+        
     }
 }
