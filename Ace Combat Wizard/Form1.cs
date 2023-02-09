01d169db4b8c1e05b61aa181ffb828326ca37b11
Andrei <46579508+Andreisgl@users.noreply.github.com>
2023-02-09 19:56:00 -0300

    Create Stream manager and PAC_Handler

--- Ace Combat Wizard/Form1.cs
+++ Ace Combat Wizard/Form1.cs
@@ -1,10 +1,33 @@
+using System.Text;
+using PAC_Utility;
 namespace Ace_Combat_Wizard
 {
     public partial class Form1 : Form
     {
+        Stream_Manager Stream_Mng = new Stream_Manager();
+        PAC_Handler PAC_Hndlr = new PAC_Handler();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string file_path = openFileDialog1.FileName;
+                label1.Text = file_path;
+
+                byte[] raw_bytes = Stream_Mng.Stream_Extractor(file_path);
+
+
+                string hex = BitConverter.ToString
+                (
+                PAC_Hndlr.TBL_reader(raw_bytes)
+                );
+                
+                label2.Text = hex;
+            }
         }
     }
 }
---
+++ Ace Combat Wizard/Libraries/PAC_Handler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PAC_Utility
+{
+	public class PAC_Handler
+	{
+		public PAC_Handler()
+		{
+		}
+
+		public byte[] TBL_reader(byte[] input_stream)
+		{
+			int index = 0;
+			int stream_length = input_stream.Length;
+			//Read number of files
+
+			byte[] n_files = new byte[4];
+			for (int i = 0; i < 4; i++)
+			{
+				n_files[i] = input_stream[index + i];
+            }
+
+
+			return n_files;
+			//byte[] return_placeholder = {(byte)0, (byte)0 };
+			//return return_placeholder;
+        }
+	}
+}
