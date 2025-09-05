d6d3ddcc190d337630b177f96b0dfbac32c78c69
Andrei <46579508+Andreisgl@users.noreply.github.com>
2025-09-05 13:40:14 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+'''Ace Combat Wizard by Andrei Segal (Andreisgl @ Github, SegalAndrei @ Twitter)
+With code by Death_the_d0g (deaththed0g @ Github, Death_the_d0g @ Twitter)
+
+This script is intended to simplify the modding process of Ace Combat 5/Zero
+It unites and automates the basic unpacking tools
+to speed up the modding process.'''
