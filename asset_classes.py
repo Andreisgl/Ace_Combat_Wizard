a1490613289d0e972f88bdad9bd4df468b8ab7c1
Andrei <46579508+Andreisgl@users.noreply.github.com>
2025-12-25 03:27:12 -0300

    create class 'Asset'

--- asset_classes.py
+++ asset_classes.py
@@ -1,18 +1,26 @@
 ''' Classes for common assets. '''
 import os
 
-class Container():
-    def __init__(self, name:str, size:int, offset, offset_table:list = []):
+class Asset():
+    def __init__(self, name:str, size:int, offset:int):
         self.name = name
-        self.offset = offset # Offset from its father container.
         self.size = size
+        self.offset = offset # Offset from its father container.
+        
+    def __repr__(self):
+        return f'ASSET | {self.name} - size={self.size} - offset={self.offset}'
 
-        self.offset_table = offset_table
+class Container(Asset):
+    def __init__(self, name:str, size:int, offset:int):
+        super().__init__(name=name, size=size, offset=offset)
+        self.offset_table = []
         self.sizes_list:list = []
         self.children = []
        
-        ref_table = offset_table[:]
-        ref_table.append(size)
+    def init_offset_table(self, offset_table:list):
+        self.offset_table = offset_table[:]
+        ref_table = self.offset_table[:]
+        ref_table.append(self.size)
 
         for i, offset in enumerate(ref_table):
             if i == len(ref_table)-1:
@@ -21,15 +29,12 @@ class Container():
             self.sizes_list.append(aux_size)
 
             name = f'{str(i).zfill(4)}.dat'
-            container = Container(name=name, offset=offset, size=aux_size)
+            container = Asset(name=name, offset=offset, size=aux_size)
             self.children.append(container)
     
     def __repr__(self):
-        return f'Container | {self.name} - size={self.size} - offset={self.offset}'
+        return f'CONTAINER | {self.name} - size={self.size} - offset={self.offset}'
 
-class DATA_PAC(Container):
-    def __init__(self, size:int, offset, offset_table:list = []):
-        pass
 
 def get_tbl_offset_table(tbl_path:str) -> list:
     # Returns the offset table and sizes from a .TBL file
@@ -48,9 +53,10 @@ def main():
     tbl_path = 'DATA.TBL'
     pac_path = 'DATA.PAC'
     tbl_offset_table = []
-    sizes_list = []
     tbl_offset_table = get_tbl_offset_table(tbl_path)
-    DATA_PAC = Container(name='DATA.PAC', size = os.stat(pac_path).st_size, offset = 0, offset_table=tbl_offset_table)
+    
+    DATA_PAC = Container(name='DATA.PAC', size = os.stat(pac_path).st_size, offset = 0)
+    DATA_PAC.init_offset_table(tbl_offset_table)
 
     pass
 
