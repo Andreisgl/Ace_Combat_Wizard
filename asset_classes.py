3c9094eed1de9e6c9d12522098407e4d7ae7d8e1
Andrei <46579508+Andreisgl@users.noreply.github.com>
2026-01-02 23:07:07 -0300

    create 'DataReference' class

--- asset_classes.py
+++ asset_classes.py
@@ -1,6 +1,67 @@
 ''' Classes for common assets. '''
 import os
 
+class DataReference():
+    ''' This class hold the actual data '''
+    def __init__(self, name:str, raw_data:bytes):
+        self._raw_data:bytes = raw_data
+        self.size = len(raw_data)
+
+        self._cursor:int = 0
+    
+    def read(self, size) -> bytes:
+        remaining_length = self.get_remaining_length()
+        if remaining_length == 0:
+            return b''
+        elif remaining_length < 0:
+            return b''
+        
+        if size > remaining_length:
+            self._cursor += remaining_length
+            return self.get_data(offset=self._cursor, length=remaining_length)
+        else:
+            self._cursor += size
+            return self.get_data(offset=self._cursor, length=size)
+        
+    def tell(self):
+        return self._cursor
+    
+    def seek(self, pos:int):
+        if not (0 < pos < self.size):
+            return self._cursor
+        
+        self._cursor = pos
+        return self._cursor
+
+    def get_remaining_length(self):
+        result = self.size - self._cursor
+        if result < 0:
+            self._cursor = self.size-1
+            return 0
+        return result
+
+    def get_data(self, offset:int, length:int) -> bytes:
+        start_address = offset
+        end_address_exclusive = start_address + length
+        output_data = b''
+
+        if start_address >= end_address_exclusive:
+            return b''
+        
+        if start_address > self.size-1:
+            return b''
+        if end_address_exclusive > self.size-1:
+            return b''
+        #
+        if start_address < 0:
+            return b''
+        if end_address_exclusive <= 0:
+            return b''
+        
+        output_data = self._raw_data[start_address:end_address_exclusive]
+        return output_data
+
+
 class Asset():
     def __init__(self, name:str, size:int, offset:int):
         self.name = name
@@ -35,7 +96,6 @@ class Container(Asset):
     def __repr__(self):
         return f'CONTAINER | {self.name} - size={self.size} - offset={self.offset}'
 
-
 def get_tbl_offset_table(tbl_path:str) -> list:
     # Returns the offset table and sizes from a .TBL file
     offset_list = []
@@ -48,35 +108,56 @@ def get_tbl_offset_table(tbl_path:str) -> list:
             size_list.append(int.from_bytes(tbl_file.read(4), byteorder="little"))
     return offset_list
 
-def get_dat_offset_table(dat_path:str): #### not done
+def get_dat_offset_table(dat_obj:Container, file:DataReference): #### not done
     offset_list = []
     zero_offset_list = []
-    with open(dat_path, 'rb') as file:
-        # Read the raw data and null offset list from the .DAT
-        read = file.read(4)            
-        number_of_files = int.from_bytes(read, byteorder="little")
-        
-        for offset_index in range(number_of_files):
-            data = file.read(4)
-            data_int = int.from_bytes(data, byteorder="little")
-            
-            if data_int != 0:
-                offset_list.append(data_int)
-            else:
-                zero_offset_list.append(offset_index)
-        
-        return offset_list, zero_offset_list
+    #with open(dat_path, 'rb') as file:
 
-            
+    file.seek(dat_obj.offset)
+    # Read the raw data and null offset list from the .DAT
+    read = file.read(4)            
+    number_of_files = int.from_bytes(read, byteorder="little")
+    
+    for offset_index in range(number_of_files):
+        data = file.read(4)
+        data_int = int.from_bytes(data, byteorder="little")
+        
+        if data_int != 0:
+            offset_list.append(data_int)
+        else:
+            zero_offset_list.append(offset_index)
+        
+    return offset_list, zero_offset_list
+
+
+
+    
+
+def get_tbl_offset_table_dataref(data:bytes):
+    offset_list = []
+    
+
 
 def main():
     tbl_path = 'DATA.TBL'
     pac_path = 'DATA.PAC'
-    tbl_offset_table = []
     tbl_offset_table = get_tbl_offset_table(tbl_path)
+
+    raw_datapac_data = b''
+    with open(pac_path, 'rb') as file:
+        raw_datapac_data = file.read()
+
+    DATA_PAC_REF = DataReference(name='datapac_ref', raw_data=raw_datapac_data)
     
-    DATA_PAC = Container(name='DATA.PAC', size = os.stat(pac_path).st_size, offset = 0)
+    DATA_PAC = Container(name='DATA.PAC', size=os.stat(pac_path).st_size, offset = 0)
     DATA_PAC.init_offset_table(tbl_offset_table)
+
+    #for dat in DATA_PAC.children:
+    #dat:Container
+    dat = DATA_PAC.children[251]
+    result = get_dat_offset_table(dat_obj=dat, file=DATA_PAC_REF)
+
+
 
     pass
 
