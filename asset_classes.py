6aefe89bb452515964adfc3d9c1c50361b3082ec
Andrei <46579508+Andreisgl@users.noreply.github.com>
2026-01-05 03:22:25 -0300

    new data workflow scheme for PAC and TBL
    Finish new table-passing scheme

--- asset_classes.py
+++ asset_classes.py
@@ -3,10 +3,11 @@ import os
 
 class DataReference():
     ''' This class hold the actual data '''
+    # TODO: Consider passing the file's path instead of copying raw data.
+    # It will save memory, make the process less verbose and mayber help with other optimizations.
     def __init__(self, name:str, raw_data:bytes):
         self._raw_data:bytes = raw_data
         self.size = len(raw_data)
-
         self._cursor:int = 0
         
     def read(self, size: int | None = None) -> bytes:
@@ -63,6 +64,35 @@ class DataReference():
         return self._raw_data[offset:end]
 
 
+class DataRefTbl(DataReference):
+    def __init__(self, name:str, raw_data:bytes):
+        super().__init__(name=name, raw_data=raw_data)
+    
+    def get_tbl_offset_table(self) -> list:
+        # Returns the offset table and sizes from a .TBL file
+        offset_list = []
+        size_list = []
+
+        #with open(tbl_path, 'rb') as self:
+        self.seek(0)
+        tbl_nof = int.from_bytes(self.read(4), byteorder="little")
+        self.seek(8)
+        for _ in range(tbl_nof):
+            offset_list.append(int.from_bytes(self.read(4), byteorder="little"))
+            size_list.append(int.from_bytes(self.read(4), byteorder="little"))
+
+        return offset_list
+
+
+class DataRefPac(DataReference):
+    def __init__(self, name:str, raw_data:bytes, tbl_ref:DataRefTbl):
+        super().__init__(name=name, raw_data=raw_data)
+        self.tbl_ref = tbl_ref
+
+    def get_offset_table(self):
+        return self.tbl_ref.get_tbl_offset_table()
+
+
 
 class Asset():
     def __init__(self, name:str, size:int, offset:int, data_ref:DataReference, index:int):
@@ -79,11 +109,13 @@ class Container(Asset):
     def __init__(self, name:str, size:int, offset:int, data_ref:DataReference, index:int):
         super().__init__(name=name, size=size, offset=offset, data_ref=data_ref, index=index)
         self.offset_table = []
-        self.sizes_list:list = [] # TODO: Check if this is used
+        self.sizes_list:list = []
         self.children = dict()
-       
-    def init_offset_table(self, offset_table:list):
+    
+    def set_offset_table(self, offset_table:list):
         self.offset_table = offset_table[:]
+
+    def init_offset_table(self):
         ref_table = self.offset_table[:]
         ref_table.append(self.size)
 
@@ -101,20 +133,13 @@ class Container(Asset):
             #self.children.append(asset)
             self.children[i] = asset
     
-    def generate_child(self,
-                       index:int,
-                       #name:str,
-                       #asset_class:type[Asset],
-                       obj:Asset):
-        
+    def generate_child(self, index:int, obj:Asset):
         '''Creates or overwrites a child asset.'''
         if index < 0 or index >= len(self.children):
             raise ValueError(f'Invalid index position: {index}/{len(self.children)}')
 
         offset = self.offset_table[index]
         size = self.sizes_list[index]
-
-        #new_asset = asset_class(name=name, size=size, offset=offset, data_ref=self.data_ref, index=index)
 
         if isinstance(obj, Asset):
             obj.offset = offset
@@ -134,14 +159,22 @@ class Container(Asset):
 
 class PacFile(Container):
     # TODO: Consider passing DATA.TBL data ref to this class on init.
-    def __init__(self, name:str, size:int, offset:int, data_ref:DataReference, index:int):
+    # TODO: Consider renaming this to 'DataPacAsset', as there are other
+    #   .PAC files with different behaviors.
+    def __init__(self, name:str, size:int, offset:int, data_ref:DataRefPac, index:int):
         super().__init__(name=name, size=size, offset=offset, data_ref=data_ref, index=index)
-        #self.offset_table = []
-        #self.sizes_list:list = []
-        #self.children = []
-       
-    def init_offset_table(self, offset_table:list):
-        self.offset_table = offset_table[:]
+        self.data_ref = data_ref # Not redundant. Receive specific DataRef class
+        self.offset_table = data_ref.get_offset_table()
+        self.init_offset_table()
+    
+    def set_offset_table(self, offset_table: list):
+        '''This method does nothing. It receives an unused param
+            to comply with the Liskov Substitution Principle'''
+        pass
+    
+    def init_offset_table(self):
+        #self.offset_table = offset_table[:]
+        self.offset_table = self.data_ref.get_offset_table()
         ref_table = self.offset_table[:]
         ref_table.append(self.size)
 
@@ -162,7 +195,6 @@ class DatFile(Container):
     def __init__(self, name:str, size:int, offset:int, data_ref:DataReference, index:int, deferred_children:bool=False):
         super().__init__(name=name, size=size, offset=offset, data_ref=data_ref, index=index)
         self.zero_offset_list = []
-        #self.generate_offset_table()
         self.dat_type:str = ''
         self.sizes_list = []
 
@@ -171,7 +203,12 @@ class DatFile(Container):
             self.generate_children()
 
     
-    def init_offset_table(self): #### TODO: Generate sizes_list
+    def set_offset_table(self, offset_table: list):
+        '''This method does nothing. It receives an unused param
+            to comply with the Liskov Substitution Principle'''
+        pass
+
+    def init_offset_table(self):
         offset_list = []
         zero_offset_list = []
         file = self.data_ref
@@ -211,18 +248,13 @@ class DatFile(Container):
                 break
             aux_size = ref_table[i+1] - ref_table[i]
             self.sizes_list.append(aux_size)
-
-    #def generate_children(self):
-    #        name = f'{str(i).zfill( len(str(len(ref_table))) )}.asset' # TODO: use len(table) for zfill
-    #        container = Asset(name=name, offset=offset, size=aux_size, data_ref=self.data_ref, index=i)
-    #        self.children.append(container)
     
     def __repr__(self):
         return f'DAT_CONTAINER | ({self.index})_{self.name} - dat_type={self.dat_type} - size={self.size} - offset={self.offset}'
 
 class DatMission(DatFile):
-    def __init__(self, name:str, size:int, offset:int, data_ref:DataReference, index:int, ace_style:str=''):
-        super().__init__(name=name, size=size, offset=offset, data_ref=data_ref, index=index)
+    def __init__(self, name:str, size:int, offset:int, data_ref:DataReference, index:int, ace_style:str='', deferred_children:bool=False):
+        super().__init__(name=name, size=size, offset=offset, data_ref=data_ref, index=index, deferred_children=deferred_children)
         self.zero_offset_list = []
         #self.generate_offset_table()
         self.dat_type:str = 'mission'
@@ -232,42 +264,27 @@ class DatMission(DatFile):
         return f'MISSION_DAT | ({self.index})_{self.name} - dat_type={self.dat_type} - size={self.size} - offset={self.offset}'
 
 
-def get_tbl_offset_table(tbl_path:str) -> list:
-    # Returns the offset table and sizes from a .TBL file
-    offset_list = []
-    size_list = []
-    with open(tbl_path, 'rb') as tbl_file:
-        tbl_nof = int.from_bytes(tbl_file.read(4), byteorder="little")
-        tbl_file.seek(8)
-        for _ in range(tbl_nof):
-            offset_list.append(int.from_bytes(tbl_file.read(4), byteorder="little"))
-            size_list.append(int.from_bytes(tbl_file.read(4), byteorder="little"))
-    return offset_list
 
-
-
-
-
-    
-
-def get_tbl_offset_table_dataref(data:bytes):
-    offset_list = []
-    
 
 
 def main():
     tbl_path = 'DATA.TBL'
     pac_path = 'DATA.PAC'
-    tbl_offset_table = get_tbl_offset_table(tbl_path)
+    
 
     raw_datapac_data = b''
     with open(pac_path, 'rb') as file:
         raw_datapac_data = file.read()
-
-    DATA_PAC_REF = DataReference(name='datapac_ref', raw_data=raw_datapac_data)
     
+    raw_datatbl_data = b''
+    with open(tbl_path, 'rb') as file:
+        raw_datatbl_data = file.read()
+
+    DATA_TBL_REF = DataRefTbl(name='datatbl_ref', raw_data=raw_datatbl_data)
+    DATA_PAC_REF = DataRefPac(name='datapac_ref', raw_data=raw_datapac_data, tbl_ref=DATA_TBL_REF)
+
     DATA_PAC = PacFile(name='DATA.PAC', size=os.stat(pac_path).st_size, offset = 0, data_ref=DATA_PAC_REF, index=0)
-    DATA_PAC.init_offset_table(tbl_offset_table)
+    #DATA_PAC.init_offset_table()
 
     
     DAT_ASSET_LIST = {
@@ -328,7 +345,7 @@ def main():
         
         if entry['dat_type'] == 'mission':
             ace_style = entry['ace_style']
-            new_child = DatMission(name=new_name, size=-1, offset=-1, data_ref=DATA_PAC.data_ref, index=int(dat_index), ace_style=ace_style)
+            new_child = DatMission(name=new_name, size=-1, offset=-1, data_ref=DATA_PAC.data_ref, index=int(dat_index), ace_style=ace_style, deferred_children=True)
             DATA_PAC.generate_child(index=int(dat_index), obj=new_child)
         #elif .... other classes...
 
